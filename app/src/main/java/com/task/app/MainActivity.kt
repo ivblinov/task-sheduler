b1d13ea114a1fe774fe1037074d478ceb1d408f3
Ivan Blinov <iv2002blinov@gmail.com>
2024-08-27 11:24:49 +0700

    TASK-2: Реализована навигация между модулями

--- app/src/main/java/com/task/app/MainActivity.kt
+++ app/src/main/java/com/task/app/MainActivity.kt
@@ -3,11 +3,21 @@ package com.task.app
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottomNav)
+            .setupWithNavController(navController)
     }
 }
