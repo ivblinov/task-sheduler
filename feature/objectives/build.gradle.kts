f54388633827a19de5161300f2f7113a5f54ff62
Ivan Blinov <iv2002blinov@gmail.com>
2024-08-27 11:24:49 +0700

    TASK-2: Реализована навигация между модулями

--- feature/objectives/build.gradle.kts
+++ feature/objectives/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    
+    // jetpack-navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
